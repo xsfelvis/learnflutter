640ef60f242a4f45cbb913d2257e0748e4ddd921
xsf <xsf_uestc_ncl@163.com>
2018-12-09 14:32:07 +0800

    @dev 添加路由功能

---
+++ lib/demo4/Demo4Main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:learnflutter/demo4/new_router.dart';
+
+class Demo4Main extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+            FlatButton(
+              child: Text("open new "),
+              textColor: Colors.blue,
+              onPressed: (){
+                Navigator.push(context, MaterialPageRoute(
+                    builder: (context){
+                      return new NewRoute();
+                }));
+              },
+
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: new Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:learnflutter/demo2/DebugConfig.dart';
 import 'package:learnflutter/demo2/Demo2Main.dart';
 import 'package:learnflutter/demo3/Demo3Main.dart';
+import 'package:learnflutter/demo4/Demo4Main.dart';
 
-//void main() => runApp(NewsApp());
 void main() {
-  runApp(Demo3Main());
+  //void main() => runApp(NewsApp());
+//  runApp(Demo3Main());
+  runApp(Demo4Main());
+
 }
